325045124475fb99b56f9b188bf04c19884f6dc3
karma <karma@iMac.local>
2017-01-11 20:10:57 +0000

    added gbk

--- dist/src/request.js
+++ dist/src/request.js
@@ -7,20 +7,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments)).next());
     });
 };
-const request = require("request-promise");
+const request = require("request");
+const iconv_1 = require("iconv");
+const charset = require("charset");
+const jschardet = require("jschardet");
+const lodash_1 = require("lodash");
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (opts) => __awaiter(this, void 0, void 0, function* () {
     console.info("request:", opts.uri);
     opts.uri = encodeURI(opts.uri);
-    return {
-        text: yield request({
+    let text = yield new Promise(function (resolve, reject) {
+        request({
             method: 'GET',
             uri: opts.uri,
             timeout: 6000,
+            encoding: null,
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
             }
-        })
+        }, function (err, res, body) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let result, buffer = body;
+                let encoding = charset(res, buffer);
+                encoding = encoding || jschardet.detect(buffer).encoding;
+                if (encoding) {
+                    console.info("Detecd charset", encoding);
+                    if (lodash_1.includes(['ascii', 'urf'], encoding)) {
+                        res.text = buffer.toString();
+                    }
+                    else {
+                        let iconv = new iconv_1.Iconv(encoding, 'UTF-8//TRANSLIT//IGNORE');
+                        res.text = iconv.convert(buffer).toString();
+                    }
+                }
+                resolve(res.text);
+            }
+        });
+    });
+    return {
+        text: text
     };
 });
 //# sourceMappingURL=request.js.map
--- src/request.js
+++ src/request.js
@@ -1,18 +1,72 @@
-import * as request from "request-promise"
+import * as request from "request"
+import { Iconv } from "iconv"
+import * as charset from 'charset'
+import * as jschardet from 'jschardet'
+import { includes } from 'lodash'
 export default async (opts)=>{
   console.info("request:", opts.uri)
   opts.uri = encodeURI(opts.uri)
   // return new Promise((resolve,reject)=>{
   //   return request.get(opts.uri).then(resolve).catch(reject)
   // })
-  return {
-    text: await request({
+  let text = await new Promise(function(resolve, reject) {
+    request({
       method: 'GET',
       uri: opts.uri,
       timeout: 6000,
+      encoding: null,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
       }
-    })
+      }, function(err, res, body){
+        if(err) {
+          reject(err)
+        } else {
+          let result, buffer = body
+          let encoding = charset(res, buffer)
+          encoding = encoding || jschardet.detect(buffer).encoding
+          // var result = iconv.decode(bufferHelper.toBuffer(),'GBK');
+          if(encoding) {
+            console.info("Detecd charset", encoding)
+            if(includes(['ascii', 'urf'], encoding)) {
+              res.text = buffer.toString()
+            } else {
+              let iconv = new Iconv(encoding, 'UTF-8//TRANSLIT//IGNORE')
+              res.text = iconv.convert(buffer).toString()
+            }
+          }
+          resolve(res.text)
+        }
+      })
+  })
+  // .on('response', function(res, err) {
+    
+  //   let buffer = []//new BufferHelper();
+  //   res.on('data', function (chunk) {
+  //     buffer.push(chunk);
+  //   });
+  //   res.on('end',function(){
+  //     let result
+  //     let encoding = charset(res, buffer)
+  //     encoding = encoding || jschardet.detect(buffer).encoding
+  //     // var result = iconv.decode(bufferHelper.toBuffer(),'GBK');
+  //     if(encoding) {
+  //       console.info("Detecd charset", encoding)
+  //       if(includes(['ascii', 'urf'],encoding)) {
+  //         res.text = buffer.toString()
+  //       } else {
+  //         let iconv = new Iconv(encoding, 'UTF-8//TRANSLIT//IGNORE')
+  //         result = iconv.convert(buffer).toString()
+  //       }
+  //     }
+  //     console.log('decoded chunk: ' + result)
+      
+  //     return result
+  //   })
+    // decompressed data as it is received
+  // })
+
+  return {
+    text: text
   }
 }
